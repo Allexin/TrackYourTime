019a83be30f8cb7afdbe797e999981c5ad18dcbd
Allexin <basovav@gmail.com>
2017-07-26 15:25:17 +0400

    Update background.js
    Change timer to interval

--- data/app_predef/chrome_extension/background.js
+++ data/app_predef/chrome_extension/background.js
@@ -1,4 +1,4 @@
-function getCurrentTabUrl(callback) {
+ffunction getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
     lastFocusedWindow: true
@@ -6,7 +6,11 @@ function getCurrentTabUrl(callback) {
 
   chrome.tabs.query(queryInfo, function(tabs) {
     var tab = tabs[0];
-    var url = tab.url;
+	if (tab == null) {
+		var url = "undefined";
+	} else {
+		var url = tab.url;
+	}
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
     callback(url);
@@ -32,7 +36,7 @@ function extractDomain(url) {
 var delimeter = ":";
 
 function sendState(url){
-    var TRACKER_INFO='PREFIX=TYTET&VERSION=1&APP_1=chrome.exe&APP_2=chromium.exe&APP_3=Google-chrome-stable&APP_4=google-chrome&APP_5=chromium-browser&APP_6=Google%20Chrome&STATE='+extractDomain(url);
+	var TRACKER_INFO='PREFIX=TYTET&VERSION=1&APP_1=chrome.exe&APP_2=chromium.exe&APP_3=Google-chrome-stable&APP_4=google-chrome&APP_5=chromium-browser&APP_6=Google%20Chrome&STATE='+extractDomain(url);
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:25856?"+TRACKER_INFO, true);
     xhr.send();     
@@ -52,8 +56,5 @@ function prepareData(){
     getCurrentTabUrl(sendState);
 }
 
-chrome.alarms.onAlarm.addListener(prepareData);
-chrome.alarms.create("TRACK_YOUR_TIME_TIMER", {
-       delayInMinutes: 0.05, periodInMinutes: 0.02}
-              );
+setInterval(prepareData, 1000);
 chrome.browserAction.setBadgeBackgroundColor({color:[0,0,0,255]});
