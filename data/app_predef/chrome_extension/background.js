bef2392280a7df7f8c428843d7a4adcf0e1d2df8
Allexin <basovav@gmail.com>
2017-11-26 22:11:08 +0400

    -Change timer in chrome extenstion from TIME to TIME SPENDED ON TAB

--- data/app_predef/chrome_extension/background.js
+++ data/app_predef/chrome_extension/background.js
@@ -1,3 +1,5 @@
+var tabsCounters = {}
+
 function getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
@@ -8,12 +10,14 @@ function getCurrentTabUrl(callback) {
     var tab = tabs[0];
 	if (tab == null) {
 		var url = "undefined";
+        var id = -1;
 	} else {
 		var url = tab.url;
+        var id = tab.id;
 	}
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
+    callback(url,id);
   });
 }
 
@@ -35,16 +39,23 @@ function extractDomain(url) {
 
 var delimeter = ":";
 
-function sendState(url){
+function sendState(url,id){
 	var TRACKER_INFO='PREFIX=TYTET&VERSION=1&APP_1=chrome.exe&APP_2=chromium.exe&APP_3=Google-chrome-stable&APP_4=google-chrome&APP_5=chromium-browser&APP_6=Google%20Chrome&STATE='+extractDomain(url);
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:25856?"+TRACKER_INFO, true);
     xhr.send();     
-    var currentTime = new Date();
-    var h = currentTime.getHours();
-    var m = currentTime.getMinutes().toString();
-    if (m.length==1) m = "0"+m;
-    var text = h+delimeter+m;
+    
+    if (typeof tabsCounters[id] === 'undefined'){
+        tabsCounters[id] = 0
+    }
+    var tabTime = tabsCounters[id] + 1
+    tabsCounters[id] = tabTime
+    
+    
+    var m = Math.floor(tabTime / 60).toString() ;
+    var s = (tabTime % 60).toString();
+    if (s.length==1) s = "0"+s;
+    var text = m+delimeter+s;
     chrome.browserAction.setBadgeText({text:text});
     if (delimeter==":")
         delimeter = " ";
